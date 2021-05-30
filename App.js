9631afa5ae3d8145bf25d86b2e388a4a33e2edf1
raihanwebmaster <raihanemon2015@gmail.com>
2021-05-30 18:04:25 +0200

    todo-app-part-3

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import { FlatList, StyleSheet, Text, View } from "react-native";
+import AddTodo from "./components/AddTodo";
 import Header from "./components/Header";
 import TodoItem from "./components/TodoItem";
 
@@ -12,18 +13,26 @@ export default function App() {
 
   const pressHandler = (key) => {
     setTodos((prevTodos) => {
-      return prevTodos.filter(todo => todo.key != key)
-    })
-  }
+      return prevTodos.filter((todo) => todo.key != key);
+    });
+  };
+
+  const submitHandler = (text) => {
+    setTodos((prevTodos) => {
+      return [{ text: text, key: Math.random().toString() }, ...prevTodos];
+    });
+  };
   return (
     <View style={styles.container}>
       <Header />
       <View style={styles.content}>
-        {/* form */}
+        <AddTodo submitHandler={submitHandler} />
         <View style={styles.list}>
           <FlatList
             data={todos}
-            renderItem={({ item }) => <TodoItem item={item} pressHandler={pressHandler} />}
+            renderItem={({ item }) => (
+              <TodoItem item={item} pressHandler={pressHandler} />
+            )}
           />
         </View>
       </View>
